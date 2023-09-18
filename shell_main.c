e1f38a9eefea013eb0f8e1c0b2ee618785d0b8c5
DoxaNtow <dianasenakey@gmail.com>
2023-09-18 05:00:54 -0700

    fixed shell_main betty

--- shell_main.c
+++ shell_main.c
@@ -2,18 +2,14 @@
 /**
  * main - entry point of shell
  * @argsCount: args counter
- * @av: args 
+ * @args: args
  * @env: environ
  * Return: 0
  */
 int main(int argsCount, char **args, char **env)
 {
-	char *getcmd = NULL; 
-	char **usercmd = NULL;
-	int pathValue = 0;
-	int _exit = 0;
-	int n = 0;
-	int i = 1;
+	char *getcmd = NULL, **usercmd = NULL;
+	int pathValue = 0, _exit = 0, n = 0, i = 1;
 	(void)argsCount;
 
 	while (i)
@@ -21,9 +17,8 @@ int main(int argsCount, char **args, char **env)
 		getcmd = _getlinecmd();
 		if (getcmd == NULL)
 		{
-			if (isatty(STDIN_FILENO)) {
+			if (isatty(STDIN_FILENO))
 				write(STDOUT_FILENO, "\n", 1);
-			}
 			exit(_exit);
 		}
 		else
@@ -35,20 +30,16 @@ int main(int argsCount, char **args, char **env)
 				free(getcmd);
 				continue;
 			}
-			if (!_strcmp(usercmd[0], "env")) {
+			if (!_strcmp(usercmd[0], "env"))
 				_getenv(env);
-			}
-			
-			if (usercmd[1] == NULL && (!_strcmp(usercmd[0], "exit"))) {
+			if (usercmd[1] == NULL && (!_strcmp(usercmd[0], "exit")))
 				_exitcmd(usercmd, getcmd, _exit);
-			}
 			else
 			{
 				n = _valuespath(&usercmd[0], env);
 				_exit = _forkcmd(usercmd, args, env, getcmd, pathValue, n);
-				if (n == 0) {
+				if (n == 0)
 					free(usercmd[0]);
-				}
 			}
 			free(usercmd);
 		}
@@ -56,3 +47,4 @@ int main(int argsCount, char **args, char **env)
 	}
 	return (_exit);
 }
+
